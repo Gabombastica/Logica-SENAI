5969efc3c08b484f625e126c18368e8fb661c222
𝐆𝐚𝐛𝐲 ୨୧ <276699980+Gabombastica@users.noreply.github.com>
2026-04-30 15:47:29 -0300

    Add files via upload

---
+++ While/Contagem Regressiva While.py
@@ -0,0 +1,10 @@
+# Hora de praticar!
+# Contagem Regressiva
+
+numero = int(input("Digite um número qualquer: "))
+while numero >= 0:
+    print(numero)
+    numero = numero - 1
+    
+print("A contagem chegou ao final!")
+    
