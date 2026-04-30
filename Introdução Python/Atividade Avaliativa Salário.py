8110135656adca8754a575e4b00fee8ef29019bf
𝐆𝐚𝐛𝐲 ୨୧ <276699980+Gabombastica@users.noreply.github.com>
2026-04-30 15:50:34 -0300

    Delete Introdução Python directory

--- Introdução Python/Atividade Avaliativa Salário.py
+++
@@ -1,6 +0,0 @@
-# Atividade Avaliativa Salário
-
-salario_atual = float(input("Olá, adicione seu salário atual para a realização do cálculo: "))
-salario_divisao = salario_atual / 10
-resultado = salario_atual + salario_divisao
-print(f" Seu novo salário é igual a R${resultado} reais e teve um aumento de R${salario_divisao} reais, ou seja, uma aumento de 10%.")
--- Introdução Python/Atividade usuário com o valor ao quadrado.py
+++
@@ -1,6 +0,0 @@
-# Hora de praticar
-# Atividade 4
-
-numero = float(input("Olá, digite um número: "))
-resultado_multiplicacao = numero * numero
-print(f"O resultado do número ao quadrado é de {resultado_multiplicacao}")
