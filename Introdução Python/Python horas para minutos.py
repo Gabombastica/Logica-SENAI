f299312734961707c1edcb3948dd97dd5835be79
𝐆𝐚𝐛𝐲 ୨୧ <276699980+Gabombastica@users.noreply.github.com>
2026-04-30 15:37:09 -0300

    Add files via upload

---
+++ Introdução Python/Python horas para minutos.py
@@ -0,0 +1,6 @@
+# Python horas para minutos
+
+numero_horas = int(input("Digite um número para representar o número de horas: "))
+multiplicacao = numero_horas * 60
+resultado = multiplicacao
+print(f"{numero_horas} horas equivalem à {multiplicacao} minutos.")
