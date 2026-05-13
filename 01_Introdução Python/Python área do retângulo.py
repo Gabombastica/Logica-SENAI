7370ecc40b44b0ccaf2bca30b4ee443644bfeea9
𝐆𝐚𝐛𝐲 ୨୧ <276699980+Gabombastica@users.noreply.github.com>
2026-05-13 13:54:57 -0300

    Delete 01_Introdução Python directory

--- 01_Introdução Python/Python área do retângulo.py
+++
@@ -1,7 +0,0 @@
-# Python área do retângulo
-
-comprimento_retangulo = float(input("Digite um número para representar o comprimento de um retângulo: "))
-largura_retangulo = float(input("Digite outro número para representar a largura do retângulo: "))
-multiplicacao_retangulo = comprimento_retangulo * largura_retangulo
-area_quadrado = multiplicacao_retangulo
-print(f"O resultado da área do retângulo é de {area_quadrado} cm².")
