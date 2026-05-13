f7338ebb9a178bc429e3e0ec92ae992b95e178c4
𝐆𝐚𝐛𝐲 ୨୧ <276699980+Gabombastica@users.noreply.github.com>
2026-05-13 14:24:42 -0300

    Add files via upload

---
+++ 02_Estrutura Condicional Python/Python Dias da Semana If Else.py
@@ -0,0 +1,30 @@
+# Atividade Prática Fluxograma
+# Dias da Semana
+
+dia = int(input("Digite um número de 1 a 7 para representar os dias da semana: "))
+
+# Verificar os dias da semana
+if dia == 1:
+    print("É Domingo!")
+    
+elif dia == 2:
+    print("É Segunda-feira!")
+
+elif dia == 3:
+    print("É Terça-feira!")
+    
+elif dia == 4:
+    print("É Quarta-feira!")
+    
+elif dia == 5:
+    print("É Quinta-feira!")
+    
+elif dia == 6:
+    print("É Sexta-feira!")
+    
+elif dia == 7:
+    print("É Sábado!")
+    
+else:
+    print("Se você não sabe, os dias da semana vão de 1 a 7, tchongo.")
+    
