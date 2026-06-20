028eedc188f100fa98eb97ddabf72fcfe8115481
𝐆𝐚𝐛𝐲 ୨୧ <276699980+Gabombastica@users.noreply.github.com>
2026-06-20 00:33:25 -0300

    Delete 01_Introdução Python directory

--- 01_Introdução Python/Python Dúzia.py
+++
@@ -1,6 +0,0 @@
-# Python dúzia
-
-unidades = int(input("Digite um número para representar o número de unidades: "))
-resultado_divisao = unidades / 12
-resultado_final = resultado_divisao
-print(f"O resultado da conversão de unidades para dúzia é de {resultado_final}.")
